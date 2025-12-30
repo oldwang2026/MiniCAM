677f88e3f1b629f80dfbf9d8c1dd1bb0edeee238
Wang Xiaowei <wangxiaowei2026@163.com>
2025-12-30 13:45:28 +0800

    添加项目文件。

---
+++ Core/include/GeoTypes.h
@@ -0,0 +1,28 @@
+﻿// MIniCamCore.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+
+struct Point3D
+{
+	double x, y, z;
+	Point3D() : x(0.0), y(0.0), z(0.0) {}
+	Point3D(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+
+};
+
+using Vector3D = Point3D;
+
+struct Triangle
+{
+	Vector3D norm;
+	Point3D v1;
+	Point3D v2;
+	Point3D v3;
+};
+
+
+
+// TODO: 在此处引用程序需要的其他标头。
---
+++ Core/src/main.cpp
@@ -0,0 +1,29 @@
+﻿// MIniCamCore.cpp: 定义应用程序的入口点。
+//
+
+#include "GeoTypes.h"
+#include <vector>
+#include <iostream>
+
+int main()
+{
+	std::cout << "MiniCAM Day 1" << std::endl;
+	std::cout << "size of Double " << sizeof(double) << "byte" << std::endl;
+	std::cout << "size of Double " << sizeof(Point3D) << "byte" << std::endl;
+	std::cout << "size of Double " << sizeof(Triangle) << "byte" << std::endl;
+
+	std::vector<Triangle> triangles;
+	triangles.reserve(3);
+
+	Triangle t1;
+	t1.norm = Vector3D(0.0, 0.0, 1.0);;
+	t1.v1 = Point3D(0, 0, 0);
+	t1.v2 = Point3D(1, 0, 0);
+	t1.v3 = Point3D(0, 1, 0);
+
+	triangles.push_back(t1);
+
+	std::cout << "Successfully created a mesh with " << triangles.size() << " triangle." << std::endl;
+
+	return 0;
+}
